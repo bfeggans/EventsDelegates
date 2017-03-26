6116e74f994f076a94c962c12c298521d0cb1470
Blake Feggans <BFeggans@reversevision.com>
2017-03-25 17:41:37 -0700

    initial commit

---
+++ src/DelegatesAndEvents/Delegates.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public delegate int WorkHandler(int workId);
+
+    public class Delegates
+    {
+        public Delegates()
+        {
+        }
+
+        public static int WorkPerformed1(int id)
+        {
+            Console.WriteLine($"Work Performed 1: {id}");
+            return id + 1;
+        }
+
+        public static int WorkPerformed2(int id)
+        {
+            Console.WriteLine($"Work Performed 2: {id}");
+            return id + 2;
+        }
+
+        public static int WorkPerformed3(int id)
+        {
+            Console.WriteLine($"Work Performed 3: {id}");
+            return id + 3;
+        }
+    }
+}
--- src/DelegatesAndEvents/Program.cs
+++ src/DelegatesAndEvents/Program.cs
@@ -9,6 +9,29 @@ namespace DelegatesAndEvents
     {
         public static void Main(string[] args)
         {
+            WorkHandler del1 = new WorkHandler(Delegates.WorkPerformed1);
+            WorkHandler del2 = new WorkHandler(Delegates.WorkPerformed2);
+            WorkHandler del3 = new WorkHandler(Delegates.WorkPerformed3);
+
+            del1 += del3 + del2;
+
+            int retVal = DoWork(del1);
+            Console.WriteLine(retVal);
+
+            Console.Read();
+        }
+
+        static int DoWork(WorkHandler del)
+        {
+            int retVal = 0;
+            //after doing some work, call my delegate
+            for (int i = 0; i < 1000; i++)
+            {
+                retVal += 1;
+                continue;
+            }
+
+            return del(retVal);
         }
     }
 }
